862473add09d5e4f33c6d27bb71e3f802cedc07c
mrt187 <babott18@tuta.io>
2026-07-03 19:25:56 +0200

    Fix supercronic fork-exec bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ python /app/sync.py
 echo "Starte Cron-Zeitplan: ${CRON_SCHEDULE}"
 printf '%s cd /app && python sync.py >> /proc/1/fd/1 2>> /proc/1/fd/2\n' "${CRON_SCHEDULE}" > /app/crontab.rendered
 
-exec supercronic /app/crontab.rendered
+exec /usr/local/bin/supercronic -no-reap /app/crontab.rendered
